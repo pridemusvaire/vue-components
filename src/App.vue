03137d1f5b40060177eeb37c51013b9afee699ff
Peter van Meijgaard <petervanmeijgaard@hotmail.com>
2016-01-06 15:30:59 +0100

    Made package standalone

--- src/App.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<router-view></router-view>
-</template>
-<script lang="babel" src="./App.js"></script>
-<style lang="stylus" src="./assets/stylus/app.styl"></style>
--- src/views/pages/home/index/index.vue
+++
@@ -1,3 +0,0 @@
-<style lang="stylus" src="./index.styl"></style>
-<template src="./index.html"></template>
-<script src="./index.js" lang="babel"></script>
