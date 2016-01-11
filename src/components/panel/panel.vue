004b4358d3932f4e4b120041814de68c08c17c7a
Peter van Meijgaard <petervanmeijgaard@hotmail.com>
2016-01-11 23:32:59 +0100

    Updates Panel

--- src/components/panel/panel.vue
+++ src/components/panel/panel.vue
@@ -1,3 +1,3 @@
-<style src="./panel.styl" lang="stylus"></style>
-<template src="./panel.html"></template>
-<script src="./panel.js" lang="babel"></script>
+<style src="./Panel.styl" lang="stylus"></style>
+<template src="./Panel.html"></template>
+<script src="./Panel.js" lang="babel"></script>
